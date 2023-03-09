8441739f1610674ee99372970321919124423a39
Nazarii Lysak <nazarlv87@gmail.com>
2023-03-09 09:15:39 +0200

    fix styles for buttons

--- src/pekemons/UI/Button/Button.jsx
+++ src/pekemons/UI/Button/Button.jsx
@@ -1,7 +1,7 @@
 import "./Button.css"
 
 const Button = ({title, active, click}) => {
-  return <button className={`element_btn ${title === active ? title : ''}`} onClick={() => click(title)}>{title}</button>
+  return <button className={`element_btn ${title} ${title === active ? title+'_active' : ''}`} onClick={() => click(title)}>{title}</button>
 };
 
 export default Button;
